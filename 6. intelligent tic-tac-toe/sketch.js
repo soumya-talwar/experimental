14f8b0f74654356809e4cf1f98b1a1b0f02cccb8
Soumya Talwar <soumya.talwar97@gmail.com>
2022-01-17 07:11:03 +0530

    intelligent tic-tac-toe

---
+++ 6. intelligent tic-tac-toe/sketch.js
@@ -0,0 +1,165 @@
+var size = 500;
+var unit = Math.floor(size / 3);
+var squares = [];
+var empty = [];
+var turns = 0;
+var symbols = ["x", "o"];
+var strike = [];
+var results = ["i won! :D", "you won! :>", "phew! that was a close match :')"];
+var over = false;
+
+var font;
+
+function preload() {
+  font = loadFont("fonts/nunito.ttf");
+}
+
+function setup() {
+  createCanvas(size, size).parent("canvas").mousePressed(pressed);
+  for (let i = 0; i < 9; i++) {
+    squares[i] = {
+      row: floor(i / 3),
+      column: i % 3,
+      empty: true,
+      player: undefined
+    };
+    empty.push(squares[i]);
+  }
+  play(random(empty), "machine");
+}
+
+function analyse(squares2, turn) {
+  let maximum = {
+    value: -Infinity
+  };
+  let minimum = {
+    value: Infinity
+  };
+  for (let i = 0; i < squares2.length; i++) {
+    if (squares2[i].empty) {
+      squares2[i].empty = false;
+      if (turn)
+        squares2[i].player = "machine";
+      else
+        squares2[i].player = "player";
+      let value;
+      let result = check(squares2);
+      if (result == results[0])
+        value = 1;
+      else if (result == results[1])
+        value = -1;
+      else if (result == results[2])
+        value = 0;
+      else
+        value = analyse(squares2, !turn).value;
+      squares2[i].empty = true;
+      squares2[i].player = undefined;
+      if (value > maximum.value) {
+        maximum.value = value;
+        maximum.index = i;
+      }
+      if (value < minimum.value) {
+        minimum.value = value;
+        minimum.index = i;
+      }
+    }
+  }
+  if (turn)
+    return maximum;
+  else
+    return minimum;
+}
+
+function play(square, player) {
+  textFont(font);
+  textSize(unit);
+  textAlign(CENTER, CENTER);
+  text(symbols[turns % 2], square.column * unit + unit / 2, square.row * unit + unit / 4);
+  square.empty = false;
+  empty.splice(empty.findIndex(element => element.row == square.row && element.column == square.column), 1);
+  square.player = player;
+  turns++;
+  if (turns > 4) {
+    let result = check(squares);
+    if (result) {
+      $("#result").html(result);
+      over = true;
+      noLoop();
+    }
+  }
+}
+
+function check(squares) {
+  for (let i = 0; i < 3; i++) {
+    let set = new Set([squares[i * 3].player, squares[i * 3 + 1].player, squares[i * 3 + 2].player]);
+    if (set.size == 1 && !set.has(undefined)) {
+      strike = [unit / 6, squares[i * 3].row * unit + unit / 2, size - unit / 6, squares[i * 3].row * unit + unit / 2];
+      if (set.has("machine"))
+        return (results[0]);
+      else
+        return (results[1]);
+    }
+  }
+  for (let i = 0; i < 3; i++) {
+    let set = new Set([squares[i].player, squares[3 + i].player, squares[6 + i].player]);
+    if (set.size == 1 && !set.has(undefined)) {
+      strike = [squares[i].column * unit + unit / 2, unit / 6, squares[i].column * unit + unit / 2, size - unit / 6];
+      if (set.has("machine"))
+        return (results[0]);
+      else
+        return (results[1]);
+    }
+  }
+  let set = new Set([squares[0].player, squares[4].player, squares[8].player]);
+  if (set.size == 1 && !set.has(undefined)) {
+    strike = [unit / 4, unit / 4, size - unit / 4, size - unit / 4];
+    if (set.has("machine"))
+      return (results[0]);
+    else
+      return (results[1]);
+  }
+  set = new Set([squares[2].player, squares[4].player, squares[6].player]);
+  if (set.size == 1 && !set.has(undefined)) {
+    strike = [size - unit / 4, unit / 4, unit / 4, size - unit / 4];
+    if (set.has("machine"))
+      return (results[0]);
+    else
+      return (results[1]);
+  }
+  let tie = true;
+  for (let square of squares) {
+    if (square.empty)
+      tie = false;
+  }
+  if (tie) {
+    strike = [];
+    return (results[2]);
+  }
+}
+
+function pressed() {
+  let row = floor(mouseY / unit);
+  let column = floor(mouseX / unit);
+  let square = squares[row * 3 + column];
+  if (turns % 2 == 1 && square.empty) {
+    play(square, "player");
+    if (empty.length > 0 && !over) {
+      let move = squares[analyse(JSON.parse(JSON.stringify(squares)), true).index];
+      console.log("final move:");
+      console.log(move);
+      setTimeout(() => play(move, "machine"), 500);
+    }
+  }
+}
+
+function draw() {
+  strokeWeight(6);
+  line(0, unit, size, unit);
+  line(0, unit * 2, size, unit * 2);
+  line(unit, 0, unit, size);
+  line(unit * 2, 0, unit * 2, size);
+  if (over) {
+    strokeWeight(8);
+    line(strike[0], strike[1], strike[2], strike[3]);
+  }
+}
