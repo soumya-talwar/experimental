be4dac6c93edf327ddefbf2837a2c55e6de75be5
Soumya Talwar <soumya.talwar97@gmail.com>
2022-02-05 17:36:07 +0530

    flappy bird

--- 10. flappy bird/sketch.js
+++ 10. flappy bird/sketch.js
@@ -1,9 +1,9 @@
 var bird = {};
 var obstacles = [];
-var colors = ["#81B214", "#CDC733"];
-var bottom = true;
-var score = 0;
+var thick = 100;
+var speed = 2.5;
 var img;
+var score = 0;
 
 function preload() {
   img = loadImage("images/bird.png");
@@ -17,57 +17,84 @@ function setup() {
 function reset() {
   score = 0;
   $("#score").html(score);
-  bird.x = 100;
-  bird.y = height / 2;
-  bird.velocity = 2;
+  bird.left = 100;
+  bird.top = height / 2;
+  bird.velocity = 0;
+  bird.gravity = 0.1;
+  bird.flight = 5;
   obstacles = [];
-  let start = 400;
-  for (let i = 0; i < 6; i++) {
-    obstacles.push({
-      color: random(colors),
-      x: start,
-      length: randomGaussian(300, 50),
-      bottom: i % 2 == 0 ? true : false
-    });
-    start += random(150, 200);
+  let start = 500;
+  for (let i = 0; i < 4; i++) {
+    let length = random(50, 300);
+    let gap = random(100, 150);
+    obstacles.push([{
+      left: start,
+      top: 0,
+      bottom: length,
+      length: length
+    }, {
+      left: start,
+      top: length + gap,
+      bottom: height,
+      length: height - length - gap
+    }]);
+    start += random(250, 300);
   }
 }
 
 function draw() {
-  background("#CDF0EA");
+  background("#C8F2EF");
   stroke(0);
   strokeWeight(3);
   for (let i = obstacles.length - 1; i >= 0; i--) {
-    let obstacle = obstacles[i];
-    fill(obstacle.color);
-    rect(obstacle.x, obstacle.bottom ? height - obstacle.length : 0, 50, obstacle.length);
-    obstacle.x -= 1;
-    if (obstacle.x + 50 < 0) {
-      obstacles.splice(i, 1);
-      obstacles.push({
-        color: random(colors),
-        x: width + random(0, 50),
-        length: randomGaussian(300, 50),
-        bottom: bottom
-      });
-      bottom = !bottom;
-      score += 100;
-      $("#score").html(score);
-    }
-    if ((bird.x + 40 > obstacle.x && bird.x < obstacle.x) || (bird.x < obstacle.x + 50 && bird.x + 40 > obstacle.x + 50)) {
-      if (!obstacle.bottom && bird.y < obstacle.length)
-        reset();
-      else if (obstacle.bottom && (bird.y + 32) > (height - obstacle.length))
-        reset();
+    for (let [index, obstacle] of obstacles[i].entries()) {
+      fill("#95CD41");
+      rect(obstacle.left, obstacle.top, thick, obstacle.length);
+      if (!(bird.left < obstacle.left && bird.left + 40 < obstacle.left) && !(bird.left > obstacle.left + thick && bird.left + 40 > obstacle.left + thick)) {
+        console.log("IN THE RANGE");
+        if (index == 0 && bird.top < obstacle.bottom)
+          reset();
+        else if (index == 1 && (bird.top + 32) > obstacle.top)
+          reset();
+      }
+      obstacle.left -= speed;
+      if (obstacle.left + thick < 0) {
+        obstacles.splice(i, 1);
+        let distance = randomGaussian(50, 20);
+        let length = random(50, 300);
+        let gap = random(100, 150);
+        obstacles.push([{
+          left: width + distance,
+          top: 0,
+          bottom: length,
+          length: length
+        }, {
+          left: width + distance,
+          top: length + gap,
+          bottom: height,
+          length: height - length - gap
+        }]);
+        break;
+      }
     }
   }
-  image(img, bird.x, bird.y);
-  bird.y = constrain(bird.y + bird.velocity, 0, height - 25);
-  bird.velocity = constrain(bird.velocity + 0.1, -5, 5);
+  bird.top += bird.velocity;
+  if (bird.top > height - 32) {
+    bird.top = height - 32;
+    bird.velocity = 0;
+  } else if (bird.top < 0) {
+    bird.top = 0;
+    bird.velocity = 0;
+  }
+  bird.velocity = constrain(bird.velocity + bird.gravity, -4, 4);
+  image(img, bird.left, bird.top);
+  if (frameCount % 5 == 0) {
+    score++;
+    $("#score").html(score);
+  }
 }
 
 function keyPressed() {
-  if (keyCode == 32) {
-    bird.velocity -= 6;
-  }
+  if (keyCode == 32)
+    bird.velocity -= bird.flight;
 }
